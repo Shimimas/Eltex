da148e89f3bf343250249054e56c98b98a997bcc
dagwynet <dagwynet@s21-school.myguest.virtualbox.org>
2023-07-17 08:10:51 +0100

    main: fix homework (n1, n2)

--- Lection9/n1.c
+++ Lection9/n1.c
@@ -2,13 +2,23 @@
 
 #define N 5
 
-int main() {
-    int counter = 1;
+void print_matrix(const int matrix[N][N]) {
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            printf("%3d ", counter++);
+            printf("%3d ", matrix[i][j]);
         }
         printf("\n");
     }
+}
+
+int main() {
+    int matrix[N][N];
+    int counter = 1;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            matrix[i][j] = counter++;
+        }
+    }
+    print_matrix(matrix);
     return 0;
 }
--- Lection9/n2.c
+++ Lection9/n2.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 
-#define N 5
+#define N 6
 
-int main() {
-    int array[N];
+void print_array(const int array[N]) {
     for (int i = 0; i < N; i++) {
-        scanf("%d", &array[i]);
-    }
-    for (int i = N - 1; i >= 0; i--) {
         printf("%d ", array[i]);
     }
     printf("\n");
+}
+
+void elements_swap(int array[N]) {
+    for (int i = 0; i < N / 2; i++) {
+        int tmp = array[i];
+        array[i] = array[N - i - 1];
+        array[N - i - 1] = tmp;
+    }
+}
+
+int main() {
+    int array[N] = {1, 2, 3, 4, 5, 6};
+    elements_swap(array);
+    print_array(array);
     return 0;
 }
